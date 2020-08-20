8ce4692cca8d453470b3a4a71222732d5bb7de7c
suranjan rai <suranjanthulungrai@gmail.com>
2020-08-20 12:27:56 +0545

    Updates

---
+++ precache-manifest.b167ee8de3d4fe10d37dff1a45299291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed41452bfa17b009b860fd287d896d62",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "e84f9daacb79164ce3f6",
+    "url": "/blog/static/css/main.cddaab28.chunk.css"
+  },
+  {
+    "revision": "3ee7aa6b28f57388640f",
+    "url": "/blog/static/js/2.bb65e8fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.bb65e8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84f9daacb79164ce3f6",
+    "url": "/blog/static/js/main.d9e3ce3f.chunk.js"
+  },
+  {
+    "revision": "77bd38835dc7b627cc32",
+    "url": "/blog/static/js/runtime-main.3703aac7.js"
+  },
+  {
+    "revision": "d5bd92fe6c932eda6f094cc8c0223112",
+    "url": "/blog/static/media/Logo.d5bd92fe.png"
+  }
+]);
