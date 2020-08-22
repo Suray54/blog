b5d9fae0aa6e1479a538d8fad8f3706346fa43c9
suranjan rai <suranjanthulungrai@gmail.com>
2020-08-22 19:47:06 +0545

    last

--- src/components/blog-overview/blog-overview.component.jsx
+++ src/components/blog-overview/blog-overview.component.jsx
@@ -20,9 +20,7 @@ const BlogOverview = ({ blogs, match, history }) => (
         <img src={html} alt="blog-overview__image" />
         <div className="blog-overview__details">
           <div
-            onClick={() =>
-              history.push(`${match.url}/${blog.title.toLowerCase()}`)
-            }
+            onClick={() => history.push(`blog/${blog.title.toLowerCase()}`)}
             className="blog-overview__details-header"
           >
             {blog.header}
